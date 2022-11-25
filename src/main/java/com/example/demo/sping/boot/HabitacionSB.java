ebac3011e1ffccc2acbc69cbefff1a9ac3b2973f
Henry17C <hicoyago@uce.edu.ec>
2022-11-25 01:51:50 -0500

    IoC y DI Spring Boot

---
+++ src/main/java/com/example/demo/sping/boot/HabitacionSB.java
@@ -0,0 +1,52 @@
+package com.example.demo.sping.boot;
+
+public class HabitacionSB {
+
+	private int numHabitacion;
+	private boolean servicioHabitacion;
+	private boolean canalesPorCable;
+	protected String tipo;
+	
+	public Integer calcularDecuento() {
+		System.out.println("Habitacion sin descuento");
+		return 0;
+	}
+	
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public int getNumHabitacion() {
+		return numHabitacion;
+	}
+	public void setNumHabitacion(int numHabitacion) {
+		this.numHabitacion = numHabitacion;
+	}
+	
+	public boolean isServicioHabitacion() {
+		return servicioHabitacion;
+	}
+	public void setServicioHabitacion(boolean servicioHabitacion) {
+		this.servicioHabitacion = servicioHabitacion;
+	}
+	public boolean isCanalesPorCable() {
+		return canalesPorCable;
+	}
+	public void setCanalesPorCable(boolean canalesPorCable) {
+		this.canalesPorCable = canalesPorCable;
+	}
+
+	@Override
+	public String toString() {
+		return "Habitacion [numHabitacion=" + numHabitacion + ", servicioHabitacion=" + servicioHabitacion
+				+ ", canalesPorCable=" + canalesPorCable + ", tipo=" + tipo + "]";
+	}
+	
+	
+	
+	
+	
+
+}
